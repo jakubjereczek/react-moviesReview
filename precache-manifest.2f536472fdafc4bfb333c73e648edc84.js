5e08e45e3e3af86950e64ec1813659029da12115
jakubjereczek <99jakubjereczek@gmail.com>
2020-07-03 16:17:30 +0200

    Updates

---
+++ precache-manifest.2f536472fdafc4bfb333c73e648edc84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70a6b7e05c9ed98f14d0fb2a0a9f72a",
+    "url": "/react-moviesReview/index.html"
+  },
+  {
+    "revision": "f784cc9c5f6346ff5a84",
+    "url": "/react-moviesReview/static/css/main.dda7c0ba.chunk.css"
+  },
+  {
+    "revision": "3b8421c9b2912a0d788d",
+    "url": "/react-moviesReview/static/js/2.b525d00f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-moviesReview/static/js/2.b525d00f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f784cc9c5f6346ff5a84",
+    "url": "/react-moviesReview/static/js/main.c9a8f81f.chunk.js"
+  },
+  {
+    "revision": "748960e7c53c5cfd4ad4",
+    "url": "/react-moviesReview/static/js/runtime-main.b0982b75.js"
+  }
+]);
